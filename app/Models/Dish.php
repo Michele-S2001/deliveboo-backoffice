a2e86ae48dd1672f8ef64b4f5e38396c3f849278
MicheleSerafini <serafini.michele01@gmail.com>
2024-01-30 18:29:14 +0100

    ADD > Withpivot() method

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -14,8 +14,6 @@ class Dish extends Model
     }
 
     public function orders (){
-
-        return $this->belongsToMany(Order::class);
-
+        return $this->belongsToMany(Order::class)->withPivot('quantity');
     }
 }
