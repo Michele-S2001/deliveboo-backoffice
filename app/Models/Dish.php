00ed91bc0cd3e8601394538715758b939f00b777
EmilianoAprile <emilianoaprile77@icloud.com>
2024-01-30 18:08:54 +0100

    Add > orders table & relations with dishes

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -12,4 +12,10 @@ class Dish extends Model
     public function restaurant (){
         return $this->belongsTo(Restaurant::class);
     }
+
+    public function orders (){
+
+        return $this->belongsToMany(Order::class);
+
+    }
 }
