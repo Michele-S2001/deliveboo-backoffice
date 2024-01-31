e6d691215632c1f3f3ba6172e64162e90ca9594c
MicheleSerafini <serafini.michele01@gmail.com>
2024-01-31 16:42:24 +0100

    FEAT > OrderSeeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -16,21 +16,21 @@ class OrderSeeder extends Seeder
     public function run(Faker $faker): void
     {
         $restaurants_ids = Restaurant::pluck('id');
-        
-        $orders_name =  
-            
-                ['Mario Rossi', 
-                'Luca Parmitano', 
-                'Delio Rossi', 
-                'Valentino Rossi', 
-                'Francesco Totti'];
+
+        $orders_name =
+
+            ['Mario Rossi',
+            'Luca Parmitano',
+            'Delio Rossi',
+            'Valentino Rossi',
+            'Francesco Totti'];
 
         for ($i = 0; $i < count($orders_name); $i++) {
             // Qui prendiamo un id random tra quelli dei ristoranti
             $current_restaurant_id = $faker -> randomElement($restaurants_ids);
             // Recuperiamo QUALCOSA 😂 l'istanza del ristorante tramite id
             $current_restaurant = Restaurant::find($current_restaurant_id);
-            $dish_ids = $current_restaurant -> dishes->pluck('id'); 
+            $dish_ids = $current_restaurant -> dishes->pluck('id');
 
             $new_order = new Order ([
                 'full_name' => $orders_name[$i],
@@ -42,19 +42,18 @@ class OrderSeeder extends Seeder
                 'payment_status' => 1,
             ]);
 
-                
             $total = 0;
-            foreach ($dish_ids as $dish_id) {
-                $dish = Dish::find($dish_id);
-                // $quantity = $dish -> pivot -> $quantity;
-                $total .= $dish->price;
-            }
-            $new_order -> subtotal = $total;
-        
-            $new_order -> save();
 
-            $new_order->dishes()
-            ->attach($faker -> randomElements($dish_ids), ['quantity' => $faker -> numberBetween(1, 4)]);
+            foreach ($dish_ids as $dish_id) {
+                $quantity = $faker->numberBetween(1, 4);
+                $total += Dish::find($dish_id)->price * $quantity;
+            }
+
+            $new_order->subtotal = $total;
+            $new_order->save();
+
+            // Collega i piatti all'ordine dopo averlo salvato
+            $new_order->dishes()->attach($dish_ids, ['quantity' => $quantity]);
         }
     }
 
