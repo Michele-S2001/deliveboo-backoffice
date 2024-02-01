168a51c2bbe5b282958fe69d979cecf9046d1612
MicheleSerafini <serafini.michele01@gmail.com>
2024-02-01 13:30:51 +0100

    ADD > Validation rules for create form

---
+++ app/Http/Requests/Admin/StoreDishRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreDishRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:200',
+            'image' => 'required|file|mimes:jpg,jpeg,png,svg,tmp|max:15000',
+            'description' => 'required|string|min:10|max:5000',
+            'price' => 'required|decimal:2|min:1.00|max:99.99',
+            'visibility' => 'boolean'
+        ];
+    }
+}
