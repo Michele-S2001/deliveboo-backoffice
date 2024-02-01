b3646fb1f8fe361568c9e7113f8dc9ac33612f5b
MicheleSerafini <serafini.michele01@gmail.com>
2024-02-01 13:31:51 +0100

    EDIT > Put some links under condition

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,36 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('Aggiungi Ristorante') }}</div>
+    <div class="container">
+        <h2 class="fs-4 text-secondary my-4">
+            {{ __('Dashboard') }}
+        </h2>
+    </div>
+    <section class="restaurant-overview">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">{{ __('Ristorante') }}</div>
 
-                <div class="card-body">
-                    @if (auth()->check() && !(auth()->user()->restaurant))
-                    <a href="{{ route('admin.restaurants.create')}}">Aggiungi Ristorante</a>
-                        @else
-                        <h3>
-                            {{ Auth::user()->restaurant->name }}
-                        </h3>
-                        <p>
-                            Indirizzo: {{ Auth::user()->restaurant->address }}
-                        </p>
-                        <p>
-                            Partita iva: {{ Auth::user()->restaurant->vat }}
-                        </p>
-                        <p>
-                            {{ Auth::user()->restaurant->category }}
-                        </p>
-                        <img class="w-100" src="{{ asset('storage/' . Auth::user()->restaurant->thumb ) }}">
-                    @endif
+                        <div class="card-body">
+                            @if (auth()->check() && !(auth()->user()->restaurant))
+                            <a href="{{ route('admin.restaurants.create')}}">Aggiungi Ristorante</a>
+                                @else
+                                <div class="row">
+                                    <div class="col-6">
+                                        <h3>
+                                            {{ Auth::user()->restaurant->name }}
+                                        </h3>
+                                        <p>
+                                            Indirizzo: {{ Auth::user()->restaurant->address }}
+                                        </p>
+                                        <p>
+                                            Partita iva: {{ Auth::user()->restaurant->vat }}
+                                        </p>
+                                        <p>
+                                            {{ Auth::user()->restaurant->category }}
+                                        </p>
+                                    </div>
+                                    <div class="col-6">
+                                        <img class="w-100" src="{{ asset('storage/' . Auth::user()->restaurant->thumb ) }}">
+                                    </div>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
+
+    @if(Auth::user()->restaurant)
+    <section class="dish-tools py-3">
+        <div class="container">
+            <a class="btn btn-primary" href="{{route('admin.dishes.create')}}">Aggiungi piatto</a>
+        </div>
+    </section>
+    @endif
 @endsection
