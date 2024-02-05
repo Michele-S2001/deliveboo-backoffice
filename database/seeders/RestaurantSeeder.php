3c9b84a4ceb8c5a765f11e024050d7ddf78275da
MicheleSerafini <serafini.michele01@gmail.com>
2024-02-05 10:19:30 +0100

    EDIT > Restaurant and User seeders

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -17,43 +17,79 @@ class RestaurantSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(Faker $faker): void
-    {   
+    {
         $users = User::all();
         $users_ids = $users->pluck('id');
 
         $categories = Category::all();
         $categories_ids = $categories->pluck('id');
-        
+
         $restaurants = [
             [
                 'name' => 'La Trattoria Ciccio',
-                'address' => 'Via Santa Maria',
+                'address' => 'Via Santa Maria, 345',
                 'vat' => '84635295843'
             ],
             [
                 'name' => 'Pizzeria da Gennarino',
-                'address' => 'Via I Maggio',
+                'address' => 'Via I Maggio, 13',
                 'vat' => '17483926574'
             ],
             [
                 'name' => 'Lucky Sushi',
-                'address' => 'Viale Toscana',
+                'address' => 'Viale Toscana, 1234',
                 'vat' => '28506756375'
+            ],
+            [
+                'name' => 'Da Michele',
+                'address' => 'Via Giacomo d\'ancona, 65',
+                'vat' => '58438463529'
+            ],
+            [
+                'name' => 'American bbq',
+                'address' => 'Via emiolio, 12',
+                'vat' => '17482657439'
+            ],
+            [
+                'name' => 'Sushi mania',
+                'address' => 'Viale Lombardia, 90',
+                'vat' => '06756375285'
+            ],
+            [
+                'name' => 'Pizzeria super buona',
+                'address' => 'Via paolo grande, 32',
+                'vat' => '84643352958'
+            ],
+            [
+                'name' => 'Magna tutto',
+                'address' => 'Via cellulare, 21',
+                'vat' => '92657417483'
+            ],
+            [
+                'name' => 'E andiamo !',
+                'address' => 'Viale fois, 320',
+                'vat' => '20006756375'
+            ],
+            [
+                'name' => 'Assaporaa',
+                'address' => 'Viale Puglia, 1245',
+                'vat' => '22504433331'
             ],
         ];
 
-        for ($i = 0; $i < count($users); $i++) { 
+        for ($i = 0; $i < count($users); $i++) {
             $new_restaurant = new Restaurant([
                 'name' => $restaurants[$i]['name'],
                 'address' => $restaurants[$i]['address'],
                 'vat' => $restaurants[$i]['vat'],
                 'user_id' => $users_ids[$i],
+                'thumb' => 'uploads/a2Ajt3H8sK4kmryfMIEjH4wQVH7zDccq1UoUXu9h.jpg',
                 'slug' => Str::slug($restaurants[$i]['name']),
             ]);
-            
-            $new_restaurant -> save();
 
-            $new_restaurant -> categories()->attach($faker -> randomElements($categories_ids, $faker -> numberBetween(1, 3)));
+            $new_restaurant->save();
+
+            $new_restaurant->categories()->attach($faker->randomElements($categories_ids, $faker->numberBetween(1, 3)));
         }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -31,8 +31,41 @@ class UserSeeder extends Seeder
                 'email' => 'emilianoaprile@gmail.com',
                 'password' => 'admin1234',
             ],
-
-
+            [
+                'name' => 'Debora Chiarelli',
+                'email' => 'deborachiarelli@gmail.com',
+                'password' => 'admin1234',
+            ],
+            [
+                'name' => 'Gianfranco Piersanti',
+                'email' => 'gianfrancopiersanti@gmail.com',
+                'password' => 'admin1234',
+            ],
+            [
+                'name' => 'Francesco Franco',
+                'email' => 'francescofranco@gmail.com',
+                'password' => 'admin1234',
+            ],
+            [
+                'name' => 'Marco Tonnarelli',
+                'email' => 'marcotonnarelli@gmail.com',
+                'password' => 'admin1234',
+            ],
+            [
+                'name' => 'Steve Jobs',
+                'email' => 'stevejobs@gmail.com',
+                'password' => 'admin1234',
+            ],
+            [
+                'name' => 'Alessandro Magno',
+                'email' => 'alessandromagno@gmail.com',
+                'password' => 'admin1234',
+            ],
+            [
+                'name' => 'Giuseppe Sedano',
+                'email' => 'giuseppesedano@gmail.com',
+                'password' => 'admin1234',
+            ]
         ];
         foreach ($users as &$user) {
             $user['password'] = Hash::make($user['password']);
