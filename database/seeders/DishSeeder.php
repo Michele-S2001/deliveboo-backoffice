1550c9606d4c4b6464e71ba3349f39e3949b7e8f
MicheleSerafini <serafini.michele01@gmail.com>
2024-02-05 10:59:55 +0100

    REFACTOR > DishSeeder

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -16,17 +16,21 @@ class DishSeeder extends Seeder
     public function run(Faker $faker): void
     {
         $restaurants = Restaurant::all();
-        $restaurants_ids = $restaurants-> pluck ('id');
+        $restaurants_ids = $restaurants->pluck('id');
 
-        for ($i = 0; $i < 10; $i++) {
-            $new_dish = new Dish ([
-                'name' => $faker -> sentence(2, true),
-                'description' => $faker -> paragraph(3),
-                'price' => $faker -> randomFloat(2, 4, 25),
-                'restaurant_id' => $faker -> randomElement($restaurants_ids),
-            ]);
+        foreach($restaurants_ids as $restaurant) {
 
-            $new_dish -> save();
+            for ($i = 0; $i < 5; $i++) {
+                $new_dish = new Dish ([
+                    'name' => $faker -> sentence(2, true),
+                    'description' => $faker -> paragraph(3),
+                    'price' => $faker -> randomFloat(2, 4, 25),
+                    'restaurant_id' => $restaurant,
+                ]);
+
+                $new_dish -> save();
+            }
+
         }
     }
 }
