e01604e625e89974a03a4f44ebcddf7c1e69905e
DamianoL <damianolunardo@hotmail.it>
2024-02-07 11:29:09 +0100

    Add - validation client for dishes

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -5,30 +5,24 @@
 
     <div class="container py-4">
         <div class="row justify-content-center">
-            <div class="col-8">
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-
-                <form action="{{route('admin.dishes.store')}}" method="POST" enctype="multipart/form-data">
+            <div class="col-md-8">
+                <form id="create-dish-form" action="{{route('admin.dishes.store')}}" method="POST" enctype="multipart/form-data">
                     @csrf
 
                     {{-- name --}}
                     <div class="mb-3">
                         <label for="name" class="form-label">Nome del piatto</label>
-                        <input required type="text" name="name" class="form-control" id="name" placeholder="Nome.." value="{{ old('name')}}">
+                        <input required type="text" name="name" class="form-control"   id="name" placeholder="Nome.." value="{{ old('name')}}">
+                       
+                        <span id="name-error" class="text-danger"></span>
                     </div>
 
                     {{-- image --}}
                     <div class="mb-3">
                         <label for="image" class="form-label">Immagine</label>
                         <input required type="file" name="image" class="form-control" id="image" placeholder="Foto del piatto.." value="{{ old('image')}}">
+                       
+                        <span id="image-error" class="text-danger"></span>
                     </div>
 
                     {{-- price --}}
@@ -36,14 +30,18 @@
                         <label class="form-label" for="price"> Prezzo </label>
                         <div class="input-group">
                             <span class="input-group-text">&euro;</span>
-                            <input required name="price" type="number" step="0.01" class="form-control" aria-label="Amount (to the nearest dollar)" value="{{old('price')}}">
+                            <input required name="price" type="number" step="0.01" class="form-control" id="price" aria-label="Amount (to the nearest dollar)" value="{{old('price')}}">
                         </div>
+                       
+                        <span id="price-error" class="text-danger"></span>
                     </div>
 
                     {{-- description --}}
                     <div class="mb-3">
                         <label for="description" class="form-label">Descrizione</label>
                         <textarea required name="description" class="form-control" id="description" rows="3"> {{ old('description') }} </textarea>
+                       
+                        <span id="description-error" class="text-danger"></span>
                     </div>
 
                     {{-- visibility --}}
@@ -54,12 +52,14 @@
                               Visibilità
                             </label>
                         </div>
+                      
+                        {{-- <span id="visibility-error" class="text-danger"></span> --}}
                     </div>
 
                     <div class="d-flex gap-3">
                         {{-- btn --}}
                         <div class="mb-3">
-                            <input type="submit" value="Salva" class="btn btn-primary">
+                            <input type="submit" value="Salva" class="btn btn-primary" id="submit-btn">
                         </div>
 
                         {{-- btn indietro --}}
@@ -67,11 +67,74 @@
                             <a class="btn btn-secondary" href=" {{route ('admin.dashboard') }}">Indietro</a>
                         </div>
                     </div>
-
+                   
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
                 </form>
 
             </div>
         </div>
     </div>
+@endsection
+
+
+@section ('script')
+<script>
+ document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("submit-btn").addEventListener('click', function() {
+        const name = document.getElementById('name').value;
+        const image = document.getElementById('image').files.length;
+        const price = document.getElementById('price').value;
+        const description = document.getElementById('description').value;
+        // const visibility = document.getElementById('visibility').checked;
+        const nameError = document.getElementById('name-error');
+        const imageError = document.getElementById('image-error');
+        const priceError = document.getElementById('price-error');
+        const descriptionError = document.getElementById('description-error');
+        // const visibilityError = document.getElementById('visibility-error');
+
+        if (name === "") {
+            nameError.innerHTML = "Il campo nome è obbligatorio";
+        } else {
+            nameError.innerHTML = "";
+        }
+
+        if (image === 0) {
+            imageError.innerHTML = "Il campo immagine è obbligatorio";
+        } else {
+            imageError.innerHTML = "";
+        }
+
+        if (price === "") {
+            priceError.innerHTML = "Il campo prezzo è obbligatorio";
+        } else {
+            priceError.innerHTML = "";
+        }
+
+        if (description === "") {
+            descriptionError.innerHTML = "Il campo descrizione è obbligatorio";
+        } else {
+            descriptionError.innerHTML = "";
+        }
+
+        
+
+        if (name !== "" && image > 0 && price !== "" && description !== "") {
+            document.getElementById('create-dish-form').submit();
+        }
+    });
+});
+</script>
+
+
+
+
 
 @endsection
