9b6ebca0e065fb48aec9dfb9bffb2801d9a8bfc7
Marta Scardillo <martascardillo02@gmail.com>
2024-02-08 19:30:30 +0100

    Fixed lables and login color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="container-fluid vh-100 hamburger">
     <div class="row justify-content-center">
-        <div class="col-4">
+        <div class="col-xl-4 col-md-6 col-11">
             <div class="border-card mt-4">
                 <div class="card-access">{{ __('Accedi') }}</div>
 
@@ -14,9 +14,9 @@
 
                         <div class="row">
                             <div class="form-outline mb-4">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
                                 <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo e-mail') }}</label>
+
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                 <span class="invalid-feedback form-label" role="alert">
@@ -28,9 +28,9 @@
 
                         <div class="mb-4 row">
                             <div class="">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
                                 <label for="password" class="form-label">{{ __('Password') }}</label>
+
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -53,7 +53,7 @@
 
                             <div class="col">
                                 @if (Route::has('password.request'))
-                                    <a class="link-offset-2" href="{{ route('password.request') }}">
+                                    <a class="" href="{{ route('password.request') }}">
                                         {{ __('Dimenticato la password?') }}
                                     </a>
                                 @endif
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="container-fluid vh-100 hamburger">
     <div class="row justify-content-center">
-        <div class="col-4">
+        <div class="col-xl-4 col-md-6 col-11">
             <div class="border-card mt-4">
                 <div class="card_header">{{ __('Registrazione') }}</div>
 
@@ -14,10 +14,10 @@
 
                         <div class="row">
                             <div class="form-outline mb-4">
-                                <input required id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"   autocomplete="name" autofocus>
-
                                 <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
+                                <input required id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"   autocomplete="name" autofocus>
+                                
                                 @error('name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -28,9 +28,9 @@
 
                         <div class="row">
                             <div class="form-outline mb-4">
-                                <input required id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"   autocomplete="email">
-
                                 <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo e-mail') }}</label>
+
+                                <input required id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"   autocomplete="email">
 
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -42,10 +42,9 @@
 
                         <div class="row">
                             <div class="form-outline mb-4">
-                                <input required id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"   autocomplete="new-password">
-                                
                                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
+                                <input required id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"   autocomplete="new-password">
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -57,9 +56,9 @@
 
                         <div class="row">
                             <div class="form-outline mb-4">
-                                <input required id="password-confirm" type="password" class="form-control" name="password_confirmation"   autocomplete="new-password">
-                                
                                 <label for="password-confirm" class="col-md-5 col-form-label text-md-right">{{ __('Conferma la Password') }}</label>
+                                
+                                <input required id="password-confirm" type="password" class="form-control" name="password_confirmation"   autocomplete="new-password">
                             </div>
                         </div>  
 
