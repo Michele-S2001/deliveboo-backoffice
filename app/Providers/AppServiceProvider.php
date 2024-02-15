3eb2aedd6d791b29cad4f21b79d2b986d5311444
EmilianoAprile <emilianoaprile77@icloud.com>
2024-02-15 16:41:41 +0100

    Add - paymente controller (TODO: make payment rules)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Braintree\Gateway;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this -> app -> singleton(
+            Gateway::class, 
+            function($app){
+                return new Gateway([
+                    'environment' => 'sandbox',
+                    'merchantId' => 'k6wtcbd6dhy78psc',
+                    'publicKey' => 'pv9zvb44h224yszk',
+                    'privateKey' => 'c9d96869718f31773d6724ea27ff98a9'
+                ]);
+            }
+        );
     }
 }
