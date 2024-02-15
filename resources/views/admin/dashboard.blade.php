65bda38932db28129a8a4dd719f06473321b4640
MicheleSerafini <serafini.michele01@gmail.com>
2024-02-15 11:55:29 +0100

    FIX > Some stuffs

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,13 +8,15 @@
                 <div class="pt-4">
                     <div class="container static-img text-white h-100 rounded">
                         @if (auth()->check() && !(auth()->user()->restaurant))
-                            <a href="{{ route('admin.restaurants.create')}}">
+                        <div class="wrap">
+                            <a class="button text-decoration-none" href="{{ route('admin.restaurants.create')}}">
                                 Aggiungi Ristorante
                             </a>
+                        </div>
                         @else
                             <div class="row h-100 pb-4">
                                 <div class="col-md-6 ps-md-5 d-flex justify-content-end flex-column">
-                                    <div class="restaurant-tag p-4">
+                                    <div class="restaurant-tag p-4 w-100">
                                         <h3>
                                             {{ Auth::user()->restaurant->name }}
                                         </h3>
@@ -28,40 +30,44 @@
                                             {{ Auth::user()->restaurant->category }}
                                         </p>
                                     </div>
-                                    
+
                                 </div>
                                 <div class="col-md-6 d-flex justify-content-center align-items-center">
                                     @if(Auth::user()->restaurant)
                                         <section class="py-3">
                                             <div class="container d-flex flex-column gap-3">
                                                 <div class="col-12">
-                                                        <a href="{{ url('profile') }}" class="button text-decoration-none">Il tuo profilo</a>
+                                                    <a href="{{ url('profile') }}" class="button text-decoration-none">Il tuo profilo</a>
                                                 </div>
 
-                                                <div class="d-flex">
-                                                    <div class="col-6 pe-2">
+                                                <div class="row">
+                                                    <div class="col-sm-6 ps-2 mb-3">
                                                         <a class="button text-decoration-none" href="{{route('admin.orders.index')}}">I tuoi ordini</a>
                                                     </div>
-                                                    
+
                                                     {{-- se l'utente con un ristorante ha anche dei piatti --}}
                                                     @if(Auth::user()->restaurant->dishes->isNotEmpty())
-                                                    <div class="col-6 ps-2">
+                                                    <div class="col-sm-6 ps-2 mb-3">
                                                         <a class="button text-decoration-none" href="{{ route('admin.dishes.index') }}">I tuoi piatti</a>
+                                                    </div>
+                                                    @else
+                                                    <div class="col-sm-6 ps-2 mb-2">
+                                                        <a href="{{ route('admin.dishes.create') }}" class="button text-decoration-none">Aggiungi un piatto</a>
                                                     </div>
                                                     @endif
                                                 </div>
-                                                
+
                                             </div>
                                         </section>
                                     @endif
                                 </div>
                             </div>
-                            
+
                         @endif
                     </div>
                 </div>
             </div>
-            
+
         </div>
     </section>
 
